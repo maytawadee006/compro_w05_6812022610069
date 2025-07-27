97e61140c5d0961406de86afc1d1378d7ec32be6
maytawadee006 <s6812022610069@email.kmutnb.ac.th>
2025-07-27 18:36:33 +0700

    .

--- coding_w05-01.c
+++ coding_w05-01.c
@@ -2,7 +2,7 @@
 
 int main(){
     int age = 18;                                     //ประกาศตัวแปรอายุ 
-    float weighs = 51.5;                              //ประกาศตัวแปรน้ำหนัก เป็นทศนิยม
+    float weighs = 55.5;                              //ประกาศตัวแปรน้ำหนัก เป็นทศนิยม
     char name = 'F';                                  //ประกาศตัวแปรชื่่อตัวพิมพ์ใหญ่
  
     printf("Student %c is %d years old.\n",name,age); //แสดงผลบรรทัดที่ 1
